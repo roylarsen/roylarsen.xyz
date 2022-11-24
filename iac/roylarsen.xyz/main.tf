d07edfdb70ea9ceff7fcb22802514494f865d296
Roy Larsen <roy.larsen@mobileheartbeat.com>
2022-11-24 16:32:44 -0500

    Adding terraform file for deployment

---
+++ iac/roylarsen.xyz/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "roylarsen.xyz.tf-backend"
+    key    = "states/roylarsen.xyz.tfstate"
+    region = "us-east-1"
+    endpoint = "nyc3.digitaloceanspaces.com"
+    skip_credentials_validation = true
+  }
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "digitalocean_project" "roylarsen.xyz" {
+  name        = "roylarsen.xyz"
+  description = "Project for Holding *.roylarsen.xyz resources"
+  purpose     = "roylarsen.xyz"
+  environment = "Development"
+}
